3c99b3b3ffd598c7a30547c5ed87f58ae0fa1164
pierre <askvader88@gmail.com>
2019-11-06 02:19:23 -0300

    primer modelos

--- apps/vendedor/models.py
+++ apps/vendedor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=70)
+    email = models.EmailField()
+
+    def __str__(self):
+        return '{} {}'.format(self.nombre, self.apellidos)
--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from apps.vendedor.models import Vendedor
 
 # Create your models here.
 
+class Venta(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+     return '{}'.format(self.nombre)
+
+
+
+
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
-    cantidad = models.CharField(max_length=50)
+    cantidad = models.FloatField(blank=True, null=True)
+    fecha_venta = models.DateField(blank=True, null=True)
+    comentario = models.TextField() 
+    vendedor = models.ForeignKey(Vendedor, null=True, blank=True, on_delete=models.CASCADE)
+    venta = models.ManyToManyField(Venta,blank=True)
+
