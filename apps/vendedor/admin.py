d86c87d02246701dad21a40706dd536de4965934
pierre <askvader88@gmail.com>
2019-11-06 23:43:28 -0300

    relaciones

--- apps/vendedor/admin.py
+++ apps/vendedor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from apps.vendedor.models import Vendedor
+
 # Register your models here.
+admin.site.register(Vendedor)
+
--- apps/ventas/admin.py
+++ apps/ventas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.ventas.models import Venta, Producto
 
 # Register your models here.
+admin.site.register(Venta)
+admin.site.register(Producto)
